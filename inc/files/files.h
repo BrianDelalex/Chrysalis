c88e2036ef36f78086456fefd75c694183ca79e7
BrianDelalex <brian.delalex@outlook.com>
2025-09-03 12:13:44 +0200

    Adding files directory in inc and src for all file related function. Adding function read_file.

---
+++ inc/files/files.h
@@ -0,0 +1,6 @@
+#ifndef FILES_H
+#define FILES_H
+
+char* read_file(const char* filepath);
+
+#endif//!FILES_H
---
+++ src/files/files.c
@@ -0,0 +1,52 @@
+# include "files/files.h"
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <errno.h>
+# include <string.h>
+
+char* read_file(const char* filepath)
+{
+    char* file;
+
+    int fd = open(filepath, O_RDONLY);
+    if (fd == -1) {
+        dprintf(2, "%s(l%d): %s\n", __FUNCTION__, __LINE__, strerror(errno));
+        return NULL;
+    }
+
+    size_t file_size = lseek(fd, 0, SEEK_END);
+    if (file_size == (size_t) -1) {
+        dprintf(2, "%s(l%d): %s\n", __FUNCTION__, __LINE__, strerror(errno));
+        close(fd);
+        return NULL;
+    }
+
+    size_t rc = lseek(fd, 0, SEEK_SET);
+    if (rc == (size_t) -1) {
+        dprintf(2, "%s(l%d): %s\n", __FUNCTION__, __LINE__, strerror(errno));
+        close(fd);
+        return NULL;
+    }
+
+    file = malloc(sizeof(char) * (file_size + 1));
+    if (!file) {
+        dprintf(2, "%s(l%d): %s\n", __FUNCTION__, __LINE__, strerror(errno));
+        close(fd);
+        return NULL;
+    }
+
+    int r_bytes = read(fd, file, file_size);
+    if (r_bytes == -1) {
+        dprintf(2, "%s(l%d): %s\n", __FUNCTION__, __LINE__, strerror(errno));
+        close(fd);
+        free(file);
+        return NULL;
+    }
+    file[file_size] = 0;
+
+    close(fd);
+    return file;
+}

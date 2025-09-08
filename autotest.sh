710032a96ddd4803a07f4799e89a82fa40984386
BrianDelalex <brian.delalex@outlook.com>
2025-09-08 11:01:35 +0200

    Updating github workflow to check memory leaks using valgrind.

--- autotest.sh
+++ autotest.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
-make run_tests
+TEST_COMPILE_SCRIPT=tests/test_compile.sh
+TEST_COMPILE_NO_MAIN_SCRIPT=tests/test_compile_no_main.sh
+TEST_STAGE1_DIR=tests/stage1
+
+make test
 if [ $? -ne 0 ]; then
     exit 1
 fi
+
+valgrind --leak-check=full --error-exitcode=1 ./tests/test_stage1
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+valgrind --leak-check=full --error-exitcode=1 ./tests/test_stage2
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+./$TEST_COMPILE_SCRIPT $TEST_STAGE1_DIR gen_main_return_0 0
+./$TEST_COMPILE_SCRIPT $TEST_STAGE1_DIR gen_main_return_42 42
+./$TEST_COMPILE_NO_MAIN_SCRIPT $TEST_STAGE1_DIR gen_no_main
+
 exit 0

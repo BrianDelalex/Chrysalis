3860d0ceba316b8bbee1aa948d766bf1c3a49ba1
BrianDelalex <brian.delalex@outlook.com>
2025-09-12 12:26:49 +0200

    Adding main_assign_var_to_var.c in stage3 test source files.

---
+++ tests/stage3/source_files/main_assign_var_to_var.c
@@ -0,0 +1,7 @@
+int main()
+{
+    int var1 = 10;
+    int var2 = var1;
+
+    return var2;
+}
--- tests/stage3/test_stage3.c
+++ tests/stage3/test_stage3.c
@@ -6,6 +6,10 @@
 
 # include "tokenizer/tokenizer.h"
 
+# include "parser/parser.h"
+
+# include "generator/generator.h"
+
 # include "files/files.h"
 
 token_list_t *open_file_and_tokenize(const char* filepath);
@@ -18,12 +22,25 @@ token_list_t *open_file_and_tokenize(const char* filepath)
     return tokens;
 }
 
+void test_all_main_assign_var_to_var(void);
+void test_all_main_assign_var_to_var(void)
+{
+    token_list_t* tokens = open_file_and_tokenize(SOURCE_FILES("main_assign_var_to_var.c"));
+    TEST_ASSERT_NOT_NULL(tokens);
+    token_list_dump(tokens);
+    ast_program_t* prg = create_ast_struct(tokens);
+    TEST_ASSERT_NOT_NULL(prg);
+    //generator(prg, OUTPUT_ASM_FILE("gen_main_assign_var_to_var.asm"));
+    token_list_free(tokens);
+}
 void test_all_simple_add(void);
 void test_all_simple_add(void)
 {
     token_list_t* tokens = open_file_and_tokenize(SOURCE_FILES("simple_add.c"));
     TEST_ASSERT_NOT_NULL(tokens);
     token_list_dump(tokens);
+    ast_program_t* prg = create_ast_struct(tokens);
+    TEST_ASSERT_NOT_NULL(prg);
     token_list_free(tokens);
 }
 
@@ -33,6 +50,8 @@ void test_all_add3(void)
 
     token_list_t* tokens = open_file_and_tokenize(SOURCE_FILES("add3.c"));
     TEST_ASSERT_NOT_NULL(tokens);
+    ast_program_t* prg = create_ast_struct(tokens);
+    TEST_ASSERT_NOT_NULL(prg);
     token_list_dump(tokens);
     token_list_free(tokens);
 }
